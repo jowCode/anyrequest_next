d3519309a1dbf42a0d525bba6408245e9010e879
JOW <weiss@media-solutions.de>
2020-01-05 14:44:22 +0100

    changed logo

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -138,7 +138,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
         ]),
         new WebpackNotifierPlugin({
             title: 'JHipster',
-            contentImage: path.join(__dirname, 'logo-jhipster.png')
+            contentImage: path.join(__dirname, 'arq-logo.svg')
         })
     ].filter(Boolean),
     mode: 'development'
