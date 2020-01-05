246c86add16a9e237c387543a8e31e7b8e2bb309
JOW <weiss@media-solutions.de>
2020-01-05 12:39:29 +0100

    put inline alert template to external html

--- src/main/webapp/app/shared/alert/alert.component.ts
+++ src/main/webapp/app/shared/alert/alert.component.ts
@@ -3,15 +3,7 @@ import { JhiAlertService, JhiAlert } from 'ng-jhipster';
 
 @Component({
   selector: 'jhi-alert',
-  template: `
-    <div class="alerts" role="alert">
-      <div *ngFor="let alert of alerts" [ngClass]="setClasses(alert)">
-        <ngb-alert *ngIf="alert && alert.type && alert.msg" [type]="alert.type" (close)="alert.close(alerts)">
-          <pre [innerHTML]="alert.msg"></pre>
-        </ngb-alert>
-      </div>
-    </div>
-  `
+  templateUrl: 'alert.component.html'
 })
 export class AlertComponent implements OnInit, OnDestroy {
   alerts: JhiAlert[] = [];
